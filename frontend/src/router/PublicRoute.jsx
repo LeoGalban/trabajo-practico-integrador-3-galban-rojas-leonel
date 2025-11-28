4c0318ad48663c341cb020ba4bfa786c4f90d880
Leo Galban <leonelgalban13@gmail.com>
2025-11-28 12:07:56 -0300

    Se crearon ruta publicas

---
+++ frontend/src/router/PublicRoute.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+import Loading from "../components/Loading";
+
+const PublicRoute = ({ children }) => {
+  const [isChecking, setIsChecking] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/api/profile", {
+          credentials: "include",
+        });
+        setIsAuthenticated(res.ok);
+      } catch {
+        setIsAuthenticated(false);
+      } finally {
+        setIsChecking(false);
+      }
+    };
+
+    checkAuth();
+  }, []);
+
+  if (isChecking) return <Loading />;
+
+  if (isAuthenticated) return <Navigate to="/home" />;
+
+  return children;
+};
+
+export default PublicRoute;
